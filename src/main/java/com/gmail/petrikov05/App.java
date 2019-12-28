c26ea78e58be4927e5a96bcea7a1c82e655272a7
ashnarkevich <petrikov05@list.ru>
2019-12-28 19:11:33 +0300

    add maven-projest

---
+++ src/main/java/com/gmail/petrikov05/App.java
@@ -0,0 +1,13 @@
+package com.gmail.petrikov05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
